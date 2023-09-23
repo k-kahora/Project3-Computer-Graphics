bfb696c7d0be2ba88db155a40c118b98efb25970
Malcolm Kahora <mango22mjk@gmail.com>
2023-09-23 14:31:05 -0400

    Points are good

--- src/triangle.js
+++ src/triangle.js
@@ -27,10 +27,10 @@ window.onload = function init()
     var color_hollow_circle = []
     var color_filled_circle = []
     var color_rect = [
-	vec3(1,0,1),
-	vec3(0.5,0,1),
-	vec3(1,0.3,1),
-	vec3(0,0,1),
+	vec3(1,1,1),
+	vec3(0,0,0),
+	vec3(1,1,1),
+	vec3(0,0,0),
     ]
 
     var rectangle = [
@@ -57,7 +57,7 @@ window.onload = function init()
 
 
     var circle_filled = circle_calc(-0.7,0,0.1,0.3, vec3(1,0,1), color_filled_circle)
-    circle_filled_length = circle.length
+    circle_filled_length = circle_filled.length
 
     // Three Vertices        
     var final_list = circle.concat(circle_filled).concat(rectangle)
@@ -67,9 +67,8 @@ window.onload = function init()
 	var res = []
 	// TODO 0 is there so that when I get the points not the color
 	for (let shape = 0; shape < shapes.length; shape++) {
-	    console.log(shapes[shape][0])
-	    for (let point = 0; point < shapes[shape][0].length; point++) {
-		res.push(shapes[shape][0][point])
+	    for (let point = 0; point < shapes[shape].points.length; point++) {
+		res.push(shapes[shape].points[point])
 	    }
 		
 	} 
@@ -78,11 +77,39 @@ window.onload = function init()
 
     }
 
-    shapes.push([circle_filled])
-    shapes.push([circle])
-    shapes.push([rectangle])
+    var make_boxes = function(row, col) {
+	
+	
+	// Given the parameters convert 
+	//  start at -1,1 add 1/8 to each side
+	//  
+	var res = []
+	var step = 1/row
+	for (let i = 0; i < row; i++) {
+	    var x = -1	    
+	    var y = 1	    
+		res.push(vec2(x + (i * step), y))
+		res.push(vec2(x + (i * step) + step, y))
+		res.push(vec2(x + (i * step), y - step))
+		res.push(vec2(x + (i * step) + step, y - step))
+		colors.push(vec3(1,1,1))
+		colors.push(vec3(1,1,1))
+		colors.push(vec3(1,1,1))
+		colors.push(vec3(1,1,1))
+	} 
+
+	console.log(res)
+	return res
+
+    }
+
+    shapes.push({"points": circle_filled})
+    shapes.push({"points": circle})
+    shapes.push({"points": rectangle})
+    shapes.push({"points": make_boxes(8,8)})
+
     console.log(get_points(shapes))
-    
+
     // Configure WebGL   
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -121,5 +148,7 @@ function render() {
     // gl.drawArrays(gl.TRIANGLES, 3, 3);
     gl.drawArrays(gl.LINE_LOOP, 0, circle_length);
     gl.drawArrays(gl.TRIANGLE_FAN, circle_length, circle_filled_length);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 126, 4);
+    console.log(shapes[3].points.length)
+    gl.drawArrays(gl.TRIANGLE_STRIP, circle_length + circle_filled_length, 4);
+    gl.drawArrays(gl.TRIANGLE_STRIP, circle_length + circle_filled_length + 4, shapes[3].points.length );
 }
