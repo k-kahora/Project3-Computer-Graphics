8795560b0e336e77c95260630a67adeb378d225e
k-kahora <mango22mjk@gmail.com>
2023-10-26 10:04:33 -0400

    nix flake and sphere

---
+++ src/sphere.js
@@ -0,0 +1,118 @@
+"use strict";
+
+var canvas;
+var gl;
+var program;
+
+var numTimesToSubdivide = 3;
+
+var index = 0;
+
+var vertices = [];
+var colors = [];
+
+var va = vec4(0.0, 0.0, -1.0,1);
+var vb = vec4(0.0, 0.942809, 0.333333, 1);
+var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
+var vd = vec4(0.816497, -0.471405, 0.333333,1);
+
+var ctm;
+var x, y, z, dx, dy, dz;
+
+function triangle(a, b, c) {
+
+     vertices.push(a);
+     vertices.push(b);
+     vertices.push(c);
+     colors.push(vec3(0, 0, 0));colors.push(vec3(0, 0, 0));colors.push(vec3(0, 0, 0));
+
+     index += 3;
+}
+
+
+function divideTriangle(a, b, c, count) {
+    if (count > 0) {
+
+        var ab = mix( a, b, 0.5);
+        var ac = mix( a, c, 0.5);
+        var bc = mix( b, c, 0.5);
+
+        ab = normalize(ab, true);
+        ac = normalize(ac, true);
+        bc = normalize(bc, true);
+
+        divideTriangle(a, ab, ac, count - 1);
+        divideTriangle(ab, b, bc, count - 1);
+        divideTriangle(bc, c, ac, count - 1);
+        divideTriangle(ab, bc, ac, count - 1);
+    }
+    else {
+        triangle(a, b, c);
+    }
+}
+
+
+function tetrahedron(a, b, c, d, n) {
+    divideTriangle(a, b, c, n);
+    divideTriangle(d, c, b, n);
+    divideTriangle(a, d, b, n);
+    divideTriangle(a, c, d, n);
+}
+
+window.onload = function init() {
+
+    canvas = document.getElementById("gl-canvas");
+
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert("WebGL 2.0 isn't available");
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    tetrahedron(va, vb, vc, vd, 3);
+    x = 0; y = 0; z = 0;
+    dx = 0.05*Math.random();
+    dy = 0.05*Math.random();
+    dz = 0.05*Math.random();
+
+    //triangle(va, vb, vc);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var positionLoc = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    // Load the data into the GPU       
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+    var vColor = gl.getAttribLocation(program, "vColor");    
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);    
+
+    render();
+}
+
+
+function render() {
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    for( var i=0; i<index; i+=3)
+        gl.drawArrays(gl.LINE_LOOP, i, 3);
+
+}
+
